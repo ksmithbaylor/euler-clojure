46c66c0babbf455f1ccd3ac952d9e69006014555
Kevin Smith <ksmithbaylor@gmail.com>
2015-02-28 13:37:01 -0500

    finished 24

---
+++ solutions/024.clj
@@ -0,0 +1,26 @@
+(defn vec->int
+  [v]
+  (reduce + (map (partial reduce *)
+                 (map vector
+                      (reverse v)
+                      (map #(apply * (repeat % 10)) (range (count v)))))))
+
+(defn nth-permutation
+  [i n]
+  (loop [i (dec i)
+         k 0
+         orig (range n)
+         nums []]
+    (if (= k n)
+      (vec->int nums)
+      (let [f (reduce * (range 1 (- n k)))
+            d (int (/ i f))
+            x (nth orig d)]
+        (recur (mod i f)
+               (inc k)
+               (remove #(= x %) orig)
+               (conj nums x))))))
+
+(defn solution
+  []
+  (nth-permutation 1e6 10))
