685180de9519437ebd00ffecb4bf0bc231270688
Kevin Smith <ksmithbaylor@gmail.com>
2015-03-25 09:25:22 -0400

    made 4 more clear

--- solutions/004.clj
+++ solutions/004.clj
@@ -1,9 +1,11 @@
-(defn solution
-  []
-  (->> (for [x (range 1e2 1e3)
-             y (range x   1e3)] (* x y))
-       (map int)
-       (map str)
-       (filter #(= % (clojure.string/reverse %)))
-       (map #(Integer/parseInt %))
-       (apply max)))
+(defn is-palindromic? [x]
+  (let [s (str x)]
+    (= s (clojure.string/reverse s))))
+
+(defn largest-palindromic [nums]
+  (apply max (filter is-palindromic? nums)))
+
+(defn solution []
+  (largest-palindromic (for [x (range 1e2 1e3)
+                             y (range x 1e3)]
+                         (int (* x y)))))
