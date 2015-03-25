d595a459b576ff9af4edc31a70265a1aea995fcc
Kevin Smith <ksmithbaylor@gmail.com>
2015-03-25 15:57:07 -0400

    added comments, cleaned up formatting, and added attribution to 7

--- solutions/007.clj
+++ solutions/007.clj
@@ -1,10 +1,23 @@
-(defn primes "Generates an infinite, lazy sequence of prime numbers"
-  ([] (primes {} 2))
-  ([table prime]
-   (let [reinsert (fn [table x factor] (update-in table [(+ factor x)] conj factor))]
-     (if-let [factors (table prime)]
-       (recur (reduce #(reinsert %1 prime %2) (dissoc table prime) factors) (inc prime))
-       (lazy-seq (cons prime (primes (assoc table (* prime prime) (vector prime)) (inc prime))))))))
+; Adapted from http://diditwith.net/2009/01/20/YAPESProblemSevenPart2.aspx
+; and http://stackoverflow.com/a/7625207
+(defn lazy-primes
+  "Generates an infinite, lazy sequence of prime numbers"
+  ([] (lazy-primes {} 2))
+  ([table cand] ; table maps primes to their factors.
+   ; reinsert "adds" factor to the list at table[factor + x].
+   (let [reinsert (fn [table x factor]
+                    (update-in table [(+ factor x)] conj factor))]
+     ; if table contains the current candidate, "remove" it and
+     ; reinsert all of its factors. if the table doesn't contain
+     ; the candidate (meaning it is prime), emit it from the lazy
+     ; sequence and "add" its square to the table.
+     (if-let [factors (table cand)]
+       (recur (reduce #(reinsert %1 cand %2)
+                      (dissoc table cand)
+                      factors)
+              (inc cand))
+       (lazy-seq (cons cand (lazy-primes (assoc table (* cand cand) (vector cand))
+                                         (inc cand))))))))
 
 (defn solution []
-  (last (take 10001 (primes))))
+  (nth (lazy-primes) 10000))
