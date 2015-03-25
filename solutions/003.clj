a747b21e2e16c5d9fd33183d93e79f3aae4e55ba
Kevin Smith <ksmithbaylor@gmail.com>
2015-03-25 09:02:39 -0400

    made 3 more clear

--- solutions/003.clj
+++ solutions/003.clj
@@ -1,12 +1,8 @@
-(defn lpf [x]
+(defn largest-prime-factor [x]
   (loop [x x i 3]
-    (cond (> i (Math/sqrt x))
-            x
-          (zero? (rem x i))
-            (recur (/ x i) i)
-          :else
-            (recur x (+ i 2)))))
+    (cond (> i (Math/sqrt x)) x
+          (zero? (rem x i)) (recur (/ x i) i)
+          :else (recur x (+ i 2)))))
 
-(defn solution
-  []
-  (lpf 600851475143))
+(defn solution []
+  (largest-prime-factor 600851475143))
