13635c0bf1d41fd34866166de107c71c5cb3395f
Kevin Smith <ksmithbaylor@gmail.com>
2015-03-25 22:36:03 -0400

    use clojure.core.reducers in 8

--- solutions/008.clj
+++ solutions/008.clj
@@ -1,11 +1,13 @@
+(require '[clojure.core.reducers :as r])
+
 (defn largest-consecutive-sum
   [length digits]
-  (let [digits (vec digits)]
-    (reduce max (for [i (range (- (count digits) (dec length)))]
-           (reduce * (subvec digits i (+ i length)))))))
+  (reduce max 0 (r/map #(r/fold * %)
+                       (for [i (range (- (count digits)
+                                         (dec length)))]
+                         (subvec digits i (+ i length))))))
 
-(defn solution
-  []
+(defn solution []
   (->> (slurp "inputs/008.txt")
        (remove (partial = \newline))
        (map #(Integer/parseInt (str %)))
